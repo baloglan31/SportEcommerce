7bedab6972aca7ee56c607610d99ac3a708f8981
muhammadeg-apa101-wq <muhammadeg-apa101-wq@code.edu.az>
2026-02-17 22:47:19 +0400

    category crud

--- SportShop/SportShop/Areas/Admin/Controllers/CategoryController.cs
+++ SportShop/SportShop/Areas/Admin/Controllers/CategoryController.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using SportShop.Data;
 using SportShop.Models;
+using SportShop.ViewModels.CategoryVM;
 
 namespace SportShop.Areas.Admin.Controllers
 {
@@ -20,24 +21,87 @@ namespace SportShop.Areas.Admin.Controllers
             return View(await _context.Categories.ToListAsync());
         }
 
-
         public IActionResult Create()
         {
             return View();
         }
 
-
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create(Category category)
+        public async Task<IActionResult> Create(CategoryVM model) 
         {
             if (ModelState.IsValid)
             {
+                var category = new Category
+                {
+                    Name = model.Name
+                };
+
                 _context.Add(category);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            return View(category);
+            return View(model);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var category = await _context.Categories.FindAsync(id);
+            if (category == null) return NotFound();
+
+            var model = new CategoryVM
+            {
+                Id = category.Id,
+                Name = category.Name
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(CategoryVM model)
+        {
+            if (ModelState.IsValid)
+            {
+                var category = await _context.Categories.FindAsync(model.Id);
+
+                if (category == null) return NotFound();
+
+                category.Name = model.Name;
+
+                _context.Update(category);
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
+            return View(model);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var category = await _context.Categories.FirstOrDefaultAsync(m => m.Id == id);
+            if (category == null) return NotFound();
+
+            return View(category); 
+        }
+
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var category = await _context.Categories.FindAsync(id);
+            if (category != null)
+            {
+                _context.Categories.Remove(category);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToAction(nameof(Index));
         }
     }
 }
---
+++ SportShop/SportShop/ViewModels/CategoryVM/CategoryVM.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SportShop.ViewModels.CategoryVM
+{
+    public class CategoryVM
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Kateqoriya adı mütləq yazılmalıdır")]
+        [StringLength(50, ErrorMessage = "Maksimum 50 simvol ola bilər")]
+        [Display(Name = "Kateqoriya Adı")]
+        public string Name { get; set; }
+    }
+}
