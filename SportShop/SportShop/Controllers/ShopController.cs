28a0f4eda85c68d75e274acdc4270f1bf618b3b4
muhammadeg-apa101-wq <muhammadeg-apa101-wq@code.edu.az>
2026-02-22 15:41:24 +0400

    shop, shop detail add

---
+++ SportShop/SportShop/Controllers/ShopController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SportShop.Data;
+using SportShop.ViewModels;
+using SportShop.ViewModels.ProductVMs;
+
+namespace SportShop.Controllers
+{
+    public class ShopController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ShopController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index(int? categoryId)
+        {
+            var categories = await _context.Categories.ToListAsync();
+
+            var productsQuery = _context.Products
+                .Include(p => p.Category)
+                .Include(p => p.Images)
+                .AsQueryable();
+
+
+            if (categoryId != null)
+            {
+                productsQuery = productsQuery.Where(p => p.CategoryId == categoryId);
+            }
+
+            var products = await productsQuery
+                .OrderByDescending(p => p.Id)
+                .Select(p => new ProductListVM
+                {
+                    Id = p.Id,
+                    Name = p.Name,
+                    Price = p.Price,
+                    CategoryName = p.Category.Name,
+                    MainImageUrl = p.Images.FirstOrDefault(i => i.IsMain).Url
+                }).ToListAsync();
+
+            var model = new ShopVM
+            {
+                Categories = categories,
+                Products = products,
+                SelectedCategoryId = categoryId
+            };
+
+            return View(model);
+        }
+
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var product = await _context.Products
+                .Include(p => p.Category)
+                .Include(p => p.Images)
+                .FirstOrDefaultAsync(p => p.Id == id);
+
+            if (product == null) return NotFound();
+
+            return View(product);
+        }
+    }
+}
---
+++ SportShop/SportShop/ViewModels/ShopVM.cs
@@ -0,0 +1,12 @@
+﻿using SportShop.Models;
+using SportShop.ViewModels.ProductVMs;
+
+namespace SportShop.ViewModels
+{
+    public class ShopVM
+    {
+        public List<Category> Categories { get; set; }
+        public List<ProductListVM> Products { get; set; }
+        public int? SelectedCategoryId { get; set; }
+    }
+}
