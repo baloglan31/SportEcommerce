9e415d7529669efd490a04455ad2f3adca13968f
muhammadeg-apa101-wq <muhammadeg-apa101-wq@code.edu.az>
2026-02-23 21:29:54 +0400

    fixed some issues with cart and new alert popup

--- SportShop/SportShop/wwwroot/assets/js/custom.js
+++ SportShop/SportShop/wwwroot/assets/js/custom.js
@@ -1,54 +1,50 @@
 ﻿document.addEventListener('DOMContentLoaded', function () {
+    toastr.options = {
+        "closeButton": true,
+        "progressBar": true,
+        "positionClass": "toast-top-right",
+        "timeOut": "3000"
+    };
 
     const addToCartBtns = document.querySelectorAll('.btn-add-to-cart');
 
-    const tokenMeta = document.querySelector('meta[name="csrf-token"]');
-    const csrfToken = tokenMeta ? tokenMeta.getAttribute('content') : null;
-
     addToCartBtns.forEach(function (btn) {
         btn.addEventListener('click', function (e) {
-            e.preventDefault(); 
-
+            e.preventDefault();
 
             const productId = this.getAttribute('data-product-id');
-            const quantity = 1; 
-
             const requestData = {
                 productId: parseInt(productId),
-                quantity: quantity
+                quantity: 1
             };
-
 
             fetch('/Cart/AddToCart', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
-                    'Accept': 'application/json',
-                    'RequestVerificationToken': csrfToken
+                    'Accept': 'application/json'
                 },
                 body: JSON.stringify(requestData)
             })
-                .then(response => {
-                    if (!response.ok) throw new Error("Server xətası!");
-                    return response.json();
-                })
+                .then(response => response.json())
                 .then(data => {
                     if (data.success) {
-
                         const cartCounter = document.getElementById('cart-counter');
                         if (cartCounter) {
                             cartCounter.innerText = data.cartCount;
                         }
 
-     
-                        alert(data.message);
+                        
+                        toastr.success(data.message, "Success!");
+
                     } else {
-                        alert("Xəta: " + data.message);
+                        
+                        toastr.error(data.message, "Something went wrong. Please try again!");
                     }
                 })
                 .catch(error => {
-                    console.error('Fetch xətası:', error);
-                    alert("Gözlənilməz xəta baş verdi.");
+                    console.error('Error:', error);
+                    toastr.error("Something went wrong in system.", "Error!");
                 });
         });
     });
